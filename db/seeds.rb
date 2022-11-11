290abcdf65a380fc34594f4d87cbe4bdc64b60ac
Joyce Chan <joycehwchan@gmail.com>
2022-11-11 13:06:24 +0900

    add lines in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 require 'open-uri'
+puts 'Destroying movies...'
 Movie.destroy_all
 
 # the Le Wagon copy of the API
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 response = JSON.parse(URI.open(url).read)
 
+puts 'Creating movies...'
 response['results'].each do |movie_hash|
   Movie.create!(
     title: movie_hash['title'],
@@ -13,23 +15,26 @@ response['results'].each do |movie_hash|
     rating: movie_hash['vote_average']
   )
 end
+puts "Created #{Movie.count} movies."
 
-List.destroy_all
-List.create!(name: "Action")
-List.create!(name: "Animation")
-List.create!(name: "Comedy")
-List.create!(name: "Crime")
-List.create!(name: "Drama")
-List.create!(name: "Fantasy")
-List.create!(name: "Historical")
-List.create!(name: "Horror")
-List.create!(name: "Romance")
-List.create!(name: "Sci-fi")
-List.create!(name: "Thriller")
-List.create!(name: "Documentary")
+# List.destroy_all
+# List.create!(name: "Action")
+# List.create!(name: "Animation")
+# List.create!(name: "Comedy")
+# List.create!(name: "Crime")
+# List.create!(name: "Drama")
+# List.create!(name: "Fantasy")
+# List.create!(name: "Historical")
+# List.create!(name: "Horror")
+# List.create!(name: "Romance")
+# List.create!(name: "Sci-fi")
+# List.create!(name: "Thriller")
+# List.create!(name: "Documentary")
 
+puts 'Destroying bookmarks...'
 Bookmark.destroy_all
 
+puts 'Creating bookmarks...'
 120.times do
   Bookmark.create(
     comment: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4),
@@ -37,3 +42,4 @@ Bookmark.destroy_all
     list: List.all.sample
   )
 end
+puts "Created #{Bookmark.count} bookmarks."
