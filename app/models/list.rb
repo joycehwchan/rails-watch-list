21f8c60ff6446cfbefdf006be1b44ee24a2cdb75
Joyce Chan <joycehwchan@gmail.com>
2022-11-11 11:40:39 +0900

    add Cloudinary

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
   # association
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :photo
   # validation
   validates :name, presence: true, uniqueness: true
 end
